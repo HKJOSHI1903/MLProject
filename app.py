88a454a11922c4834064537e80ce549b7f7384fe
HKJOSHI1903 <harshitjoshi769@gmail.com>
2025-08-03 06:38:13 +0530

    web work

--- app.py
+++ app.py
@@ -10,11 +10,8 @@ app = application
 predict_pipeline = PredictPipeline()
 
 ## Route for a home page
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
@@ -44,4 +41,4 @@ def predict_datapoint():
             return render_template('home.html', error=f"Prediction failed: {e}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True) # Use debug=True for development
+    app.run(host="0.0.0.0",debug=True) # Use debug=True for development
